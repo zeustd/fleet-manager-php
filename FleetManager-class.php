0a27f0c069751e18d4b7bec4eb02dc5c32c5097a
Zeus The Developer <75146431+zeustd@users.noreply.github.com>
2022-12-14 13:12:21 -0500

    Create FleetManager-class.php

---
+++ FleetManager-class.php
@@ -0,0 +1,32 @@
+<?php
+
+class FleetManager {
+  private $conn; // database connection object
+
+  // constructor
+  public function __construct($db_conn) {
+    $this->conn = $db_conn;
+  }
+
+  // method to add a new vehicle to the fleet
+  public function addVehicle($make, $model, $year, $vin) {
+    $query = "INSERT INTO fleet (make, model, year, vin) VALUES (?, ?, ?, ?)";
+    $stmt = $this->conn->prepare($query);
+    $stmt->bind_param("ssis", $make, $model, $year, $vin);
+    $stmt->execute();
+    return $stmt->insert_id;
+  }
+
+  // method to get a list of all vehicles in the fleet
+  public function getFleet() {
+    $query = "SELECT * FROM fleet";
+    $result = $this->conn->query($query);
+    $fleet = array();
+    while ($row = $result->fetch_assoc()) {
+      $fleet[] = $row;
+    }
+    return $fleet;
+  }
+}
+
+?>
