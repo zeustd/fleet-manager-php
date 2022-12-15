8748e0e64ce070b8b117452378a4d4a9c93828a8
Zeus The Developer <75146431+zeustd@users.noreply.github.com>
2022-12-15 13:04:01 -0500

    Update example.php

--- example.php
+++ example.php
@@ -7,13 +7,16 @@ $db_conn = new mysqli("localhost", "user", "password", "database");
 $fleet_manager = new FleetManager($db_conn);
 
 //you can use the addVehicle method to add a new vehicle to the fleet:
-$fleet_manager->addVehicle("Honda", "Accord", 2020, "123456789ABCDEFG");
+$vehicle_id = $fleet_manager->addVehicle("Honda", "Accord", 2020, "123456789ABCDEFG");
 
 //You can also use the getFleet method to retrieve a list of all vehicles in the fleet:
 $fleet = $fleet_manager->getFleet();
 foreach ($fleet as $vehicle) {
   echo $vehicle['year'] . " " . $vehicle['make'] . " " . $vehicle['model'] . "\n";
 }
+
+// record a gas purchase for the vehicle
+$purchase_id = $fleetManager->recordGasPurchase($vehicle_id, "2022-12-15", 1234, 50.75, '{"gallons": 10}', "gas");
 
 
 ?>
